a757db3e4c547c05784aebd3b968caa4d96bdff9
Thierry Moudiki <thierry.moudiki@gmail.com>
2023-12-17 19:30:08 +0000

    update example cor test

--- examples/esgcortest.py
+++ examples/esgcortest.py
@@ -3,20 +3,10 @@ import os
 from esgtoolkit import esgcortest, simshocks
 from time import time
 
-
-n = 500
-
-s0_par1 = simshocks(n = n, horizon = 3, frequency = "semi-annual",
+n = 100
+s0_par1 = simshocks(n = n, horizon = 4, frequency = "semi-annual",
 family = 1, par = 0.2, par2= 0)
-
-s0_par2 = simshocks(n = n, horizon = 3, frequency = "semi-annual", 
+s0_par2 = simshocks(n = n, horizon = 4, frequency = "semi-annual", 
 family = 1, par = 0.8, par2 = 0)
-
 print(esgcortest(s0_par1))
-
 print(esgcortest(s0_par2))
-
-
-print(esgcortest(s0_par1, return_R_obj = True))
-
-print(esgcortest(s0_par2, return_R_obj=True))
